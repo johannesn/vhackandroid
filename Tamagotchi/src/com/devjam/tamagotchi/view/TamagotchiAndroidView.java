0465a6f80ae43cd6103aff71b9784237c956bcec
quidon.ho <quidon.ho@99879fba-28d1-966d-9dfb-36683da0148d>
2012-10-06 19:48:56 +0000

    customizable head, torso

--- Tamagotchi/src/com/devjam/tamagotchi/view/TamagotchiAndroidView.java
+++ Tamagotchi/src/com/devjam/tamagotchi/view/TamagotchiAndroidView.java
@@ -15,14 +15,16 @@ import com.devjam.tamagotchi.game.Monster;
 
 public class TamagotchiAndroidView extends View {
 
-//	public static final int MONSTERDENSITY = 360;
-//	public static final int MONSTERDENSITY = 360;
+	// public static final int MONSTERDENSITY = 360;
+	// public static final int MONSTERDENSITY = 360;
 	private static final String TAG = "MyCustomView";
 
 	private Bitmap head;
 	private Bitmap torso;
+	private Bitmap torsos[];
 	private Bitmap legs;
 	private Bitmap leg[];
+	private Bitmap[] heads;
 	private Paint mPaint;
 	private Game mAnimationEndListener;
 	private Monster mMonster;
@@ -46,7 +48,58 @@ public class TamagotchiAndroidView extends View {
 	public void initialize() {
 		head = BitmapFactory
 				.decodeResource(getResources(), R.drawable.head_1_a);
+		heads = new Bitmap[12];
+		heads[0] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_1_a);
+		heads[1] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_2_a);
+		heads[2] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_3_a);
+		heads[3] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_4_a);
+		heads[4] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_5_a);
+		heads[5] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_6_a);
+		heads[6] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_7_a);
+		heads[7] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_8_a);
+		heads[8] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_9_a);
+		heads[9] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_10_a);
+		heads[10] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_11_a);
+		heads[11] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.head_12_a);
+
 		torso = BitmapFactory.decodeResource(getResources(), R.drawable.body_6);
+		torsos = new Bitmap[12];
+		torsos[0] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_1);
+		torsos[1] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_2);
+		torsos[2] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_3);
+		torsos[3] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_4);
+		torsos[4] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_5);
+		torsos[5] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_6);
+		torsos[6] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_7);
+		torsos[7] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_8);
+		torsos[8] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_9);
+		torsos[9] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_10);
+		torsos[10] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_11);
+		torsos[11] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.body_12);
 
 		legs = BitmapFactory.decodeResource(getResources(), R.drawable.foot_1);
 		leg = new Bitmap[10];
@@ -71,12 +124,12 @@ public class TamagotchiAndroidView extends View {
 		leg[9] = BitmapFactory.decodeResource(getResources(),
 				R.drawable.foot_10);
 
-//		for (Bitmap b : leg)
-//			b.setDensity(MONSTERDENSITY);
-//
-//		head.setDensity(MONSTERDENSITY);
-//		torso.setDensity(MONSTERDENSITY);
-//		legs.setDensity(MONSTERDENSITY);
+		// for (Bitmap b : leg)
+		// b.setDensity(MONSTERDENSITY);
+		//
+		// head.setDensity(MONSTERDENSITY);
+		// torso.setDensity(MONSTERDENSITY);
+		// legs.setDensity(MONSTERDENSITY);
 
 		mPaint = new Paint();
 	}
@@ -100,9 +153,9 @@ public class TamagotchiAndroidView extends View {
 
 		Log.d(TAG, "Dens" + canvas.getDensity());
 
-		Bitmap headToDraw = head;
-		Bitmap torsoToDraw = torso;
-		Bitmap legsToDraw = legs;
+		Bitmap headToDraw = heads[mMonster.getHead()];
+		Bitmap torsoToDraw = torsos[mMonster.getTorso()];
+		Bitmap legsToDraw = leg[mMonster.getLegs()];
 		counter++;
 		if (counter % 40 == 0) {
 			animating = true;
@@ -140,7 +193,7 @@ public class TamagotchiAndroidView extends View {
 
 		canvas.drawBitmap(headToDraw, 0, -100, mPaint);
 		canvas.drawBitmap(torsoToDraw, 0, 335, mPaint);
-		canvas.drawBitmap(leg[mMonster.getLegs()], 0, 465, mPaint);
+		canvas.drawBitmap(legsToDraw, 0, 465, mPaint);
 	}
 
 	public void startSleepingAnimation() {
